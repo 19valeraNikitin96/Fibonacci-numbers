f5f4a74003429186d3c7e354e835c2e3de4337b0
erlang <19valeraniktiin96@gmail.com>
2020-02-03 14:37:16 +0200

    Factorial concurrent

--- src/factorial.erl
+++ src/factorial.erl
@@ -8,17 +8,33 @@
 %%%-------------------------------------------------------------------
 -module(factorial).
 -author("erlang").
+-define(PROC, 4).
 
 %% API
--export([factorial/2, test_factorial/1]).
--export([factorial1/2, factorial3_child/0, factorial3/1, factorial2/1]).
+-export([factorial/2, test_factorial/1, factorial5/1, test_factorial_concurrent/1, test_factorial_single_values/1, factorial4/1]).
+-export([factorial1/2, factorial3_child/0, factorial3/1, factorial2/1, factorial4_receiver/3, factorial4_worker/3]).
 
 test_factorial(Number)->
   {
     {first, timer:tc(factorial, factorial,[Number, first])},
-    {second, timer:tc(factorial, factorial,[Number, second])},
-    {third, timer:tc(factorial, factorial,[Number, third])}
+    {second, timer:tc(factorial, factorial,[Number, second])}
   }.
+
+test_factorial_concurrent(List)->
+  {T1, _} = timer:tc(factorial,factorial4,[List]),
+  {T2, _} = timer:tc(factorial,factorial5,[List]),
+  {
+    {fourth, T1}
+    ,
+    {fifth, T2}
+  }.
+
+test_factorial_single_values(Number)->
+  {
+    {fourth, timer:tc(factorial,factorial2,[Number])},
+    {fifth, timer:tc(factorial,fact,[Number])}
+  }.
+
 
 factorial(0, _)->1;
 factorial(Number, _)
@@ -28,7 +44,7 @@ factorial(Number, Mode)->
   case Mode of
     first  -> factorial1(Number, 1);
     second -> factorial2(Number);
-    third -> factorial3(Number);
+    third  -> factorial3(Number);
     _ -> undefined
   end.
 
@@ -36,9 +52,7 @@ factorial1(1, Acc)->Acc;
 factorial1(Number, Acc)->
   factorial1(Number-1, Acc*Number).
 
-factorial2(1)->1;
-factorial2(Number)->
-  Number*factorial2(Number-1).
+
 
 factorial3(Number)->
   spawn(factorial, factorial3_child, []) ! {prev, self(), Number},
@@ -55,9 +69,36 @@ factorial3_child()->
     {next, _From, Number} -> get(prev) ! {next, self(), get(value)*Number}
   end.
 
+%%------------------------------------
+factorial5(List)->
+  factorial5(List, []).
+factorial5([], Result)->Result;
+factorial5([H | T], Result)->
+  factorial5(T, [factorial2(H) | Result]).
 
+factorial2(1)->1;
+factorial2(Number)->
+  timer:sleep(5),
+  Number*factorial2(Number-1).
+%%------------------------------------
+factorial4(List)->
+  factorial4_receiver(List,length(List), []).
 
-
+factorial4_receiver([],0,Result)->Result;
+factorial4_receiver([],Length,Result) ->
+  receive
+    Value -> factorial4_receiver([],Length-1,[Value|Result])
+  end;
+factorial4_receiver([H|T],Length,Result) ->
+  spawn(factorial, factorial4_worker,[H,1,self()]),
+  receive
+    Value -> factorial4_receiver(T,Length-1,[Value|Result])
+    after 0 -> factorial4_receiver(T,Length,Result)
+  end.
+factorial4_worker(1, Acc, R_PID)->R_PID ! Acc;
+factorial4_worker(Number, Acc, R_PID)->
+  timer:sleep(5),
+  factorial4_worker(Number-1, Acc*Number, R_PID).
 
 
 
